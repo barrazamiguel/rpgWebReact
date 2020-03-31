0308e54ea656b016f673a0ce6332df27d88b540c
Miloro <miloromiguel@gmail.com>
2020-03-31 15:27:19 -0300

    Updates

---
+++ precache-manifest.2bb8aae30d61be823d7d87417d79b670.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cd3288bdc3ce27e6aaadd23f2d5addd",
+    "url": "/rpgWebReact/index.html"
+  },
+  {
+    "revision": "95c4e00e1e6f36f73eb4",
+    "url": "/rpgWebReact/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "61fb4c0b26a8cb7880a1",
+    "url": "/rpgWebReact/static/js/2.09775423.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/rpgWebReact/static/js/2.09775423.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95c4e00e1e6f36f73eb4",
+    "url": "/rpgWebReact/static/js/main.93cb5c8f.chunk.js"
+  },
+  {
+    "revision": "5e1f02cc456017e78e9c",
+    "url": "/rpgWebReact/static/js/runtime-main.51d4ce66.js"
+  }
+]);
