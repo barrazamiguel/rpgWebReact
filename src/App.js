ceece73b872e45e9b76bbb4f3edbb2c5565ffb6e
Miguel Barraza <miguelbarraza2015@gmail.com>
2020-03-31 18:46:04 -0300

    agregado hashrouter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { HashRouter, Route, Switch } from 'react-router-dom'
 import Home from './components/Home';
 import SeleccionarPersonaje from './components/SeleccionarPersonaje';
 import Juego from './components/Juego';
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-  
   render() {
     return (
-      <BrowserRouter>
+      <HashRouter>
       <Switch>
         <Route path="/" component={Home} exact/>
         <Route path="/select" component={SeleccionarPersonaje} exact/>
         <Route path="/game" component={Juego} exact/>
       </Switch>
-      </BrowserRouter>
+      </HashRouter>
     );
   }
 }
